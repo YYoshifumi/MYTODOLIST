db3162a177f2b179b240ba18b288c12344978db5
YYoshifumi <101238694+YYoshifumi@users.noreply.github.com>
2022-09-14 21:37:51 +0900

    Update 2022_09_10_082127_create_folders_table.php

--- database/migrations/2022_09_10_082127_create_folders_table.php
+++ database/migrations/2022_09_10_082127_create_folders_table.php
@@ -15,7 +15,7 @@ class CreateFoldersTable extends Migration
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->id();
-            $table->string('user_id');
+            $table->integer('user_id');
             $table->string('title', 20);
             $table->timestamps();
         });
