b0cd328c5aa9fce0df499f955e90b4edc7111c1c
YYoshifumi <101238694+YYoshifumi@users.noreply.github.com>
2022-09-20 18:38:37 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,21 @@
     <div class="row">
         <div class="col col-md-offset-3 col-md-6">
             <nav class="panel panel-default">
+            
+             <div class="panel-heading">
+                    loginしましょう
+                </div>
+                <div class="panel-body">
+                    <div class="text-center">
+
+                        <a href="{{ route('login') }}" class="btn btn-primary">
+                            loginページへ
+                        </a>
+                        <a href="{{ route('register') }}">会員登録はこちら！</a>
+                    </div>
+                </div>
+                
+                @if(Auth:check())
                 <div class="panel-heading">
                     まずはフォルダを作成してください。
                 </div>
@@ -15,6 +30,8 @@
                         </a>
                     </div>
                 </div>
+                @endif
+                
             </nav>
         </div>
     </div>
